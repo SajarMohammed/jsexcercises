95572a611d73f22869f9b02a38f56dc8a09912a0
sajarappuz <sajarmohammed71@gmail.com>
2024-01-02 18:21:28 +0530

    excercises edited

--- exc7.js
+++ exc7.js
@@ -1,50 +1,38 @@
-function validate(configdata, segmentdata){
-      for(let key in configdata){
-        const config = configdata[key];
-        const value = segmentdata[key];
-        
-        if(config.type === "REQUIRED" && ( value === undefined || value === null || value === "")){
-            return false
-        }
-        if(value & value.length > key.length){
-            return false
-        }
-        
-      }
-      return true
+let isTrue = true;
+
+function validate(configdata, segmentdata) {
+  for (let key in configdata) {
+    const config = configdata[key];
+    const value = segmentdata[key];
+
+    if (config.type === "REQUIRED" && (value === undefined || value === null || value === "")) {
+      isTrue = false;
+    }
+    if (value && value.length > key.length) {
+      isTrue = false;
+    }
+  }
+  // Remove the following line
+  // isTrue = true;
 }
 
 const configdata = {
-    'CO': { type: 'REQUIRED' },
-    'MAJ': { type: 'NORMAL' },
-    'SET': { type: 'NORMAL' },
-    'MIN': { type: 'REQUIRED' },
-  };
-  const passingSegments = {
-    'CO': '01',
-    'MAJ': '111',
-    'SET': '111',
-    'MIN': '000',
-  };
-  
-  const failingSegments = {
-    'CO': '',
-    'MAJ': '222',
-    'SET': '222',
-    'MIN': '111',
-  };
+  'CO': { type: 'NORMAL' },
+  'MAJ': { type: 'REQUIRED' },
+  'SET': { type: 'NORMAL' },
+  'MIN': { type: 'REQUIRED' },
+};
+const failingSegments = {
+  'CO': '',
+  'MAJ': '22',
+  'SET': '222',
+  'MIN': '11144',
+};
 
-  const keySegments = {
-    'CO': '345',
-    'MAJ': '222',
-    'SET': '222',
-    'MIN': '111',
-  };
+validate(configdata, failingSegments);
 
-  const result = validate(configdata,passingSegments)
-
-  if(result){
-    console.log("validation successfull")
-  }else{
-    console.log("not successfull")
-  }
+if (isTrue) {
+  console.log("Validation successful");
+} else {
+  console.log("Not successful");
+}
